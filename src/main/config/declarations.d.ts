ecf23fdea4e19c0dc5da8e72464d46c5c674a547
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-04 20:00:48 -0300

    chore: add declarations config

---
+++ src/main/config/declarations.d.ts
@@ -0,0 +1,19 @@
+declare module '*.txt' {
+  const value: string
+  export = value
+}
+
+declare module '*.jpg' {
+  const value: string
+  export = value
+}
+
+declare module '*.png' {
+  const value: string
+  export = value
+}
+
+declare module '*.svg' {
+  const value: string
+  export = value
+}
