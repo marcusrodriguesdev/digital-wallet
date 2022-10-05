c6c1cbe7ed9a0bece6f05f2ab822d951712712fe
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-04 21:43:29 -0300

    chore: ensure global style

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/Router'
+import { GlobalStyle } from '@/presentation/theme/GlobalStyle'
 
-ReactDOM.render(<Router />, document.getElementById('main'))
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <Router />
+  </React.Fragment>,
+  document.getElementById('main')
+)
---
+++ src/presentation/theme/GlobalStyle.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    font-size: 14px;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+`
