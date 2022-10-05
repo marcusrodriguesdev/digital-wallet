fcd060b5aa315f28d6069005b42df344def74516
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-05 15:23:27 -0300

    feat: ensure open sans font

--- src/presentation/theme/GlobalStyle.tsx
+++ src/presentation/theme/GlobalStyle.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   * {
     font-size: 14px;
+    font-family: 'Open Sans', sans-serif;
     padding: 0;
     margin: 0;
     box-sizing: border-box;
