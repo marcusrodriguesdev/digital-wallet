7a0ebffb426664f2020c155975efd8432e650a8f
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-06 16:29:05 -0300

    chore: add configuration in webpack to run svg

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,11 @@ module.exports = {
           { loader: 'css-loader', options: { modules: true } },
           { loader: 'sass-loader' }
         ]
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack', 'url-loader']
       }
     ]
   },
