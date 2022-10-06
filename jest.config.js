ecf85d405c9e8a5a9b34c2e56e5986b748aef6d0
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-06 20:04:44 -0300

    chore: remove files in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '**/*.d.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '**/*.d.ts',
+    '!<rootDir>/src/**/index.{ts,tsx}',
+    '!<rootDir>/src/**/*.stories.tsx',
+    '!<rootDir>/src/**/*.styles.tsx',
+    '!<rootDir>/src/**/tests/**/*'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
