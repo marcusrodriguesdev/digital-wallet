b42bfca39e7e7f894b6721676619b14f3a04a962
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-10 12:34:11 -0300

    feat: ensure new icons

--- src/presentation/theme/Assets.ts
+++ src/presentation/theme/Assets.ts
@@ -1,7 +1,13 @@
-import EyeClose from '../assets/icons/eye-off-outline.svg'
-import EyeOpen from '../assets/icons/eye-outline.svg'
+import ArrowGroup from '@/presentation/assets/icons/arrow-group.svg'
+import CircleGroup from '@/presentation/assets/icons/circle-group.svg'
+import ColumnGroup from '@/presentation/assets/icons/column-group.svg'
+import EyeOpen from '@/presentation/assets/icons/eye-outline.svg'
+import EyeClose from '@/presentation/assets/icons/eye-off-outline.svg'
 
 const Assets = {
+  ArrowGroup,
+  CircleGroup,
+  ColumnGroup,
   EyeClose,
   EyeOpen
 }
