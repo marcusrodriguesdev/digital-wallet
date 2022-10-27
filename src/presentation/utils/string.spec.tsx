eb8a68b2928371fccb8a39e319c8050b6c80a0fb
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-10-27 00:08:34 -0300

    test: ensure validate email function in auth screen presenter

--- src/presentation/utils/string.spec.tsx
+++ src/presentation/utils/string.spec.tsx
@@ -1,4 +1,4 @@
-import { getInitials } from './string'
+import { getInitials, validateEmail } from './string'
 
 describe('presentation/utils/string', () => {
   it('should return the initials of a string', () => {
@@ -14,5 +14,23 @@ describe('presentation/utils/string', () => {
   it('should return the null if the string is empty', () => {
     const initials = getInitials('')
     expect(initials).toBeNull()
+  })
+
+  it('should return true if the email is valid', () => {
+    const email = 'alguem@gmail.com'
+    const isValid = validateEmail(email)
+    expect(isValid).toBeTruthy()
+  })
+
+  it('should return false if the email is invalid', () => {
+    const email = 'alguemgmail.com'
+    const isValid = validateEmail(email)
+    expect(isValid).toBeFalsy()
+  })
+
+  it('should return false if the email is empty', () => {
+    const email = ''
+    const isValid = validateEmail(email)
+    expect(isValid).toBeFalsy()
   })
 })
