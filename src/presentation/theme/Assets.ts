be69d0b1da12a64d88931a327917b02336699e8e
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-11-07 15:14:29 -0300

    feat: ensure new svgs

--- src/presentation/theme/Assets.ts
+++ src/presentation/theme/Assets.ts
@@ -1,17 +1,35 @@
 import ArrowGroup from '@/presentation/assets/icons/arrow-group.svg'
+import ArrowLeft from '@/presentation/assets/icons/arrow-left.svg'
 import CircleGroup from '@/presentation/assets/icons/circle-group.svg'
 import ColumnGroup from '@/presentation/assets/icons/column-group.svg'
+import DashboardSelected from '@/presentation/assets/icons/dashboard-selected.svg'
+import Dashboard from '@/presentation/assets/icons/dashboard.svg'
 import EyeClose from '@/presentation/assets/icons/eye-off-outline.svg'
 import EyeOpen from '@/presentation/assets/icons/eye-outline.svg'
 import HamburguerMenu from '@/presentation/assets/icons/hamburguer-menu.svg'
+import LogoutSelected from '@/presentation/assets/icons/logout-selected.svg'
+import Logout from '@/presentation/assets/icons/logout.svg'
+import SettingsSelected from '@/presentation/assets/icons/settings-selected.svg'
+import Settings from '@/presentation/assets/icons/settings.svg'
+import WalletSelected from '@/presentation/assets/icons/wallet-selected.svg'
+import Wallet from '@/presentation/assets/icons/wallet.svg'
 
 const Assets = {
   ArrowGroup,
+  ArrowLeft,
   CircleGroup,
   ColumnGroup,
+  DashboardSelected,
+  Dashboard,
   EyeClose,
   EyeOpen,
-  HamburguerMenu
+  HamburguerMenu,
+  Logout,
+  LogoutSelected,
+  SettingsSelected,
+  Settings,
+  WalletSelected,
+  Wallet
 }
 
 export { Assets }
