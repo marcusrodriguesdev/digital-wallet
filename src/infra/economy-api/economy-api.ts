b18d97f55eb537bad671d6393bdf092327bbc707
Marcus Rodrigues <marcusrodriguesdev@gmail.com>
2022-12-07 10:16:00 -0300

    feat: ensure interface to api returns

--- src/infra/economy-api/economy-api.ts
+++ src/infra/economy-api/economy-api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios'
 
-const getCurrency = async (currency: string): Promise<void> => {
+import { Currency } from '@/domain/protocols/currency'
+
+/**
+ *
+ * @param currency - Currency code
+ * @returns  Currency data from API or error
+ */
+const getCurrency = async (currency: string): Promise<Currency> => {
   try {
     const response = await axios.get(`https://economia.awesomeapi.com.br/json/${currency}`)
     return response.data
